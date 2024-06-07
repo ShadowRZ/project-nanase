953c7ef465a6c35e2e5e7417726ffee13b941d36
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2024-06-07 16:07:52 +0800

    refactor: Migrate Text to Panda CSS

--- src/app/atoms/text/Text.tsx
+++ src/app/atoms/text/Text.tsx
@@ -1,28 +1,51 @@
-import { klassed } from '@klass/solid';
+import { styled } from '~styled/jsx';
 
-const Text = klassed('p', {
-  base: 'm-0 p-0',
+const Text = styled('p', {
+  base: {
+    margin: '0',
+    padding: '0',
+  },
   variants: {
     content: {
-      default: '',
-      truncate: 'min-w-0 truncate',
+      default: {},
+      truncate: {
+        minWidth: '0',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+      },
     },
     font: {
-      default: '',
-      bold: 'font-bold',
-      italic: 'font-italic',
+      default: {},
+      bold: { fontWeight: 'bold' },
+      italic: { fontStyle: 'italic' },
     },
     size: {
-      default: 'text-base',
-      large: 'text-xl',
-      medium: 'text-lg',
-      small: 'text-sm',
-      smaller: 'text-xs',
+      default: {
+        fontSize: '1rem',
+        lineHeight: '1.5rem',
+      },
+      large: {
+        fontSize: '1.25rem',
+        lineHeight: '1.75rem',
+      },
+      medium: {
+        fontSize: '1.125rem',
+        lineHeight: '1.75rem',
+      },
+      small: {
+        fontSize: '0.875rem',
+        lineHeight: '1.25rem',
+      },
+      smaller: {
+        fontSize: '0.75rem',
+        lineHeight: '1rem',
+      },
     },
     color: {
-      default: '',
-      warning: 'text-yellow',
-      error: 'text-red',
+      default: {},
+      warning: { color: 'yellow.11' },
+      error: { color: 'red.11' },
     },
   },
 });
