da77f743c5f26f319d004dbef9ab47a63453d1d1
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2024-06-27 22:33:57 +0800

    refactor: Use Node replace instead of hast to JSX

--- src/lib/utils/sanitize.ts
+++ src/lib/utils/sanitize.ts
@@ -79,57 +79,6 @@ const TAG_ALLOWED_CLASSES = {
 };
 
 function transformAnchorTag(tagName: string, attribs: Attributes): Tag {
-  const userLink = /^https?:\/\/matrix.to\/#\/(@.+:.+)/.exec(
-    decodeURIComponent(attribs.href)
-  );
-  const roomAliasLink = /^https?:\/\/matrix.to\/#\/(#.+:.+)/.exec(
-    decodeURIComponent(attribs.href)
-  );
-  const roomLink =
-    /^https?:\/\/matrix.to\/#\/(![^/]+:[^/]+)(?:\/(\$[^?/]+))?(?:\?(.+))?/.exec(
-      decodeURIComponent(attribs.href)
-    );
-  if (userLink !== null) {
-    // Convert user link to pill
-    const userId = userLink[1];
-    const pill = {
-      tagName: 'span',
-      attribs: {
-        'data-project-nanase-href': attribs.href,
-        'data-project-nanase-pill': userId,
-      },
-    };
-    return pill;
-  }
-
-  if (roomAliasLink !== null) {
-    const roomAlias = roomAliasLink[1];
-    const pill = {
-      tagName: 'span',
-      attribs: {
-        'data-project-nanase-href': attribs.href,
-        'data-project-nanase-reference': roomAlias,
-      },
-    };
-    return pill;
-  }
-
-  if (roomLink !== null) {
-    const roomId = roomLink[1];
-    const eventId = roomLink[2];
-    const vias = roomLink[3];
-    const pill = {
-      tagName: 'span',
-      attribs: {
-        'data-project-nanase-href': attribs.href,
-        'data-project-nanase-reference': roomId,
-        'data-project-nanase-reference-event': eventId,
-        'data-project-nanase-reference-vias': vias,
-      },
-    };
-    return pill;
-  }
-
   const rex =
     /[\u{1F300}-\u{1F5FF}\u{1F900}-\u{1F9FF}\u{1F600}-\u{1F64F}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F1E6}-\u{1F1FF}\u{1F191}-\u{1F251}\u{1F004}\u{1F0CF}\u{1F170}-\u{1F171}\u{1F17E}-\u{1F17F}\u{1F18E}\u{3030}\u{2B50}\u{2B55}\u{2934}-\u{2935}\u{2B05}-\u{2B07}\u{2B1B}-\u{2B1C}\u{3297}\u{3299}\u{303D}\u{00A9}\u{00AE}\u{2122}\u{23F3}\u{24C2}\u{23E9}-\u{23EF}\u{25B6}\u{23F8}-\u{23FA}]/gu;
   const newHref = attribs.href.replaceAll(
