d813012c1a0bfedc771610c2067b49dc229da98e
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2024-06-29 23:25:55 +0800

    chore: Use ESM file for Prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import('prettier').Config}
+ */
+const config = {
+  trailingComma: 'es5',
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxSingleQuote: true,
+  bracketSameLine: false,
+};
+
+export default config;
