cfc717b0ce4e89e21fd7e2939f3a662364596a4d
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2024-07-18 10:15:58 +0800

    refactor: Migrate Placeholder to Panda CSS

--- src/app/components/placeholder/Placeholder.tsx
+++ src/app/components/placeholder/Placeholder.tsx
@@ -1,14 +1,21 @@
 import { type Component } from 'solid-js';
+import { Circle, Flex, styled } from '~styled/jsx';
 
 const Placeholder: Component = () => {
   return (
-    <div class='animate-pulse flex flex-row items-center gap-2 w-full'>
-      <div class='size-12 rounded-full bg-slate-200' />
-      <div class='grow flex flex-col gap-1 overflow-hidden'>
-        <span class='bg-slate-200 rounded-lg w-1/4 h-5' />
-        <span class='bg-slate-200 rounded-lg w-1/2 h-5' />
-      </div>
-    </div>
+    <Flex
+      direction='row'
+      alignItems='center'
+      gap='2'
+      w='full'
+      animation='pulse'
+    >
+      <Circle size='12' bg='slate.3' />
+      <Flex direction='column' gap='1' grow='1' overflow='hidden'>
+        <styled.span rounded='lg' bg='slate.3' w='1/4' h='5' />
+        <styled.span rounded='lg' bg='slate.3' w='1/2' h='5' />
+      </Flex>
+    </Flex>
   );
 };
 
