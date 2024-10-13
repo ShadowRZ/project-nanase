53f922294ae6256ea3fb55c595ed53b942efb6be
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2024-10-13 15:00:43 +0800

    chore: Update TSConfig

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,5 @@
 /// <reference types="vite/client" />
 /// <reference types="unplugin-icons/types/solid" />
 /// <reference types="@types/grecaptcha" />
+
+declare module '@fontsource-variable/inter' {}
