d917bd454494928ce802006dc54c455ef713cf71
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2024-10-17 14:21:57 +0800

    chore: Update ESLint and pnpm

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from 'typescript-eslint';
 import solid from 'eslint-plugin-solid/configs/typescript';
 import comments from '@eslint-community/eslint-plugin-eslint-comments/configs';
 import unicorn from 'eslint-plugin-unicorn';
-import importX from 'eslint-plugin-import-x';
+import imporPlugin from 'eslint-plugin-import';
 import promise from 'eslint-plugin-promise';
 
 export default tseslint.config(
@@ -29,16 +29,16 @@ export default tseslint.config(
     },
   },
   eslint.configs.recommended,
-  importX.flatConfigs.recommended,
-  importX.flatConfigs.typescript,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+  imporPlugin.flatConfigs.recommended,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+  imporPlugin.flatConfigs.typescript,
   unicorn.configs['flat/recommended'],
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
   promise.configs['flat/recommended'],
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
   comments.recommended,
-  // eslint-disable-next-line import-x/no-named-as-default-member
   ...tseslint.configs.recommendedTypeChecked,
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   {
     files: ['**/*.{ts,tsx}'],
     ...solid,
@@ -46,14 +46,10 @@ export default tseslint.config(
   // Project Rules
   {
     rules: {
-      'import-x/no-unresolved': 'off',
-      'import-x/extensions': [
-        'error',
-        'never',
-        { svg: 'always', css: 'always' },
-      ],
+      'import/no-unresolved': 'off',
+      'import/extensions': ['error', 'never', { svg: 'always', css: 'always' }],
       // Tiptap extensions are imcompatiable
-      'import-x/no-named-as-default': 'off',
+      'import/no-named-as-default': 'off',
       'unicorn/prevent-abbreviations': 'off',
       'unicorn/filename-case': 'off',
       'unicorn/empty-brace-spaces': 'off',
@@ -80,7 +76,6 @@ export default tseslint.config(
   // Disable Typed lintings for Storybook configs
   {
     files: ['.storybook/*.{ts,tsx}'],
-    // eslint-disable-next-line import-x/no-named-as-default-member
     extends: [tseslint.configs.disableTypeChecked],
   }
 );
